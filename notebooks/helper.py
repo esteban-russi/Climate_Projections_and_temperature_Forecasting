f23ddcb7f0ac254c4da70b7179df0657cd8dd4f6
Esteban G. Russi <110236985+esteban-russi@users.noreply.github.com>
2025-03-25 10:33:37 +0000

    esteban_eda

--- notebooks/helper.py
+++
@@ -1,9 +0,0 @@
-import xarray as xr
-
-def load_datasets(file_paths):
-    "Load the six .nc files to return a list of xarray datasets"
-    datasets = []
-    for path in file_paths:
-        ds = xr.open_dataset(path)
-        datasets.append(ds)
-    return datasets
